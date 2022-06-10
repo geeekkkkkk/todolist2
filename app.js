ebc8fd5239fad88a28db6634b9ac5492b483ec69
geeekkkkkk <injeel.fame@gmail.com>
2022-06-10 20:33:10 +0530

    With css added

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var items = [];
 
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 
@@ -30,6 +32,6 @@ app.post("/", function(request, respond){
     respond.redirect("/");
 })
 
-app.listen(3000, function() {
-    console.log("listening on port 3000...");
+app.listen(3001, function() {
+    console.log("listening on port 3001...");
 });
